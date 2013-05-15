835854192f3b49b9abca0827df5c7c81d9ec0a75
Sam T <pxqr.sta@gmail.com>
2013-05-15 18:46:33 +0400

    - Remove some useless comments.

--- tests/Shared.hs
+++ tests/Shared.hs
@@ -23,28 +23,3 @@ swapM = method "swap" ["x", "y"] ["b", "a"]
 
 shiftR :: Method ((), Int, [Int]) ([Int], (), Int)
 shiftR = method "shiftR" ["x", "y", "z"] ["a", "b", "c"]
-
-
-
-{-
-type NodeId = Int
-type InfoHashe = Int
-type NodeAddr = Int
-type Token = Int
-type
-
-ping :: Method NodeId NodeId
-ping = method "ping" ["id"] ["id"]
-
-find_node :: Method (NodeId, NodeId) (NodeId, NodeAddr)
-find_node = method "find_node" ["id", "target"] ["id", "nodes"]
-
-get_peers :: Method (NodeId :*: InfoHash) (NodeId, Token, NodeAddr :|: NodeAddr)
-get_peers = method "get_peers"
-  ("id", "target")
-  ("id", "token", view ("values" :|: "nodes"))
-view :: BEncodable -> Maybe BEncodable
-view  = undefined
-announce_peer :: Method (NodeId, InfoHash, PortNumber, Token) NodeId
-announce_peer = undefined
--}
