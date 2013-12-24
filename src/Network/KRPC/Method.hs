10829a428735d034f927e45561dcf94703cd376a
Sam Truzjan <pxqr.sta@gmail.com>
2013-12-24 06:28:23 +0400

    Update documentation in Method module

--- src/Network/KRPC/Method.hs
+++ src/Network/KRPC/Method.hs
@@ -1,3 +1,12 @@
+-- |
+--   Copyright   :  (c) Sam Truzjan 2013
+--   License     :  BSD3
+--   Maintainer  :  pxqr.sta@gmail.com
+--   Stability   :  experimental
+--   Portability :  portable
+--
+--   Normally, you don't need to import this module.
+--
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE FunctionalDependencies     #-}
@@ -10,6 +19,7 @@ module Network.KRPC.Method
        ) where
 
 import Data.BEncode (BEncode)
+import Data.ByteString.Char8 as BC
 import Data.Char
 import Data.Monoid
 import Data.List as L
@@ -18,31 +28,30 @@ import Data.Typeable
 import Network.KRPC.Message
 
 
--- | Method datatype used to describe name, parameters and return
---   values of procedure. Client use a method to /invoke/, server
---   /implements/ the method to make the actual work.
+-- | Method datatype used to describe method name, parameters and
+-- return values of procedure. Client use a method to /invoke/, server
+-- /implements/ the method to make the actual work.
 --
 --   We use the following fantom types to ensure type-safiety:
 --
---     * param: Type of method parameters. Ordinary Tuple type used
---     to specify more than one parameter, so for example @Method
---     (Int, Int) result@ will take two arguments.
+--     * param: Type of method parameters.
 --
---     * result: Type of return value of the method. Similarly,
---     tuple used to specify more than one return value, so for
---     exsample @Method (Foo, Bar) (Bar, Foo)@ will take two arguments
---     and return two values.
+--     * result: Type of return value of the method.
 --
 newtype Method param result = Method MethodName
   deriving (Eq, Ord, IsString, BEncode)
 
+-- | Example:
+--
+--   @show (Method \"concat\" :: [Int] Int) == \"concat :: [Int] -> Int\"@
+--
 instance (Typeable a, Typeable b) => Show (Method a b) where
   showsPrec _ = showsMethod
 
 showsMethod :: forall a. forall b. Typeable a => Typeable b
             => Method a b -> ShowS
 showsMethod (Method name) =
-    shows name <>
+    showString (BC.unpack name) <>
     showString " :: " <>
     shows paramsTy <>
     showString " -> " <>
@@ -52,15 +61,23 @@ showsMethod (Method name) =
     paramsTy = typeOf (impossible :: a)
     valuesTy = typeOf (impossible :: b)
 
--- | Example:
+-- | In order to perform or handle KRPC query you need to provide
+--   corresponding 'KRPC' class.
+--
+--   Example:
+--
 --   @
 --   data Ping = Ping Text deriving BEncode
 --   data Pong = Pong Text deriving BEncode
 --
---   instance KRPC Ping Pong where
---     method = "ping"
+--   instance 'KRPC' Ping Pong where
+--     method = \"ping\"
 --   @
+--
 class (BEncode req, BEncode resp) => KRPC req resp | req -> resp where
+  -- | Method name. Default implementation uses lowercased @req@
+  -- datatype name.
+  --
   method :: Method req resp
 
   -- TODO add underscores
